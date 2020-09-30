b65a025f74085dea3ea48bdc47cf22770938be0f
Khdbble <fb503d52.af1c4d@gmail.com>
2020-09-30 15:58:03 +0900

    reduced iteration for easy debug

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas
 
 df = pandas.read_excel("inflation_ratesG7.xlsx", sheet_name="Sheet1").iloc[:, 1:].values
 
-pg = panel_garch(dataframe=df, iR=5)
+pg = panel_garch(dataframe=df, iR=2)
 # pg = panel_garch(dataframe=df, iR = 100)
 pg.run(DGP=False, debug_print=True)
 # pg.run(debug_print=True)
