4d3a86950cd9765a536d2f564f20ef2ace378ea6
Khdbble <fb503d52.af1c4d@gmail.com>
2020-09-30 14:11:29 +0900

    refactored tests

--- src/vech.py
+++ src/vech.py
@@ -17,9 +17,18 @@ def vech(mX):
     return vH
 
 
+def test_vech(mX):
+    print("before: ")
+    print(mX)
+    res = vech(mX)
+    print("after: ")
+    print(res)
+    print()
+
 # test
-# print(vech(np.ones((3, 3))))
-# print(vech(np.ones((2, 3))))
-# print(vech(np.array([[1, 2, 3],
-#                      [2, 4, 2],
-#                      [3, 21, 8]])))
+test_vech(np.ones((3, 3)))
+test_vech(np.array([[1, 2, 3],
+                   [2, 4, 2],
+                   [3, 21, 8]]))
+# fail case; non-symetric
+# test_vech(np.ones((2, 3)))
