218ed1b6e778c7553d1b5b51eb747281316e0a29
Khdbble <fb503d52.af1c4d@gmail.com>
2020-10-14 22:56:17 +0900

    resolved dependency on DGP

--- src/DGP.py
+++ src/DGP.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-from scipy.linalg import sqrtm
+import scipy
 from .unvech import unvech
 
 
@@ -9,37 +9,42 @@ def DGP(vTheta, vAlpha, vSigma, vLambda, iT, iN, iI):
     # > https://docs.python.org/2/library/random.html
     random.seed(123 + 2 * iI)
 
-    assert len(vTheta) == 2, "vTheta not length 2"
     phi, beta = vTheta
-
+    gam, rho, varphi, eta = vLambda
     mSig = unvech(vSigma)
 
-    assert len(vLambda) == 4, "vLambda not length 4"
-    gam, rho, varphi, eta = vLambda
+    # Definition (13)
+    mC = np.full((iN, iN), rho) + \
+        (gam - rho) * np.identity(iN)
+    mD = np.full((iN, iN), eta) + \
+        (varphi - eta) * np.identity(iN)
 
-    # Definition (7)
-    mC = np.full((iN, iN), rho) + (gam - rho) * np.identity(iN)
-    mD = np.full((iN, iN), eta) + (varphi - eta) * np.identity(iN)
-
+    # Equation (14)
     mK = mSig - np.dot(np.dot(mC, mSig), mC) - np.dot(np.dot(mD, mSig), mD)
 
     # Matrix with size (iT * iN), elements are generated with Uniform dist over [0, 1)
     mX = np.random.rand(iT, iN)
 
+    # Equation (15)
     vMy = (1 / (1 - phi)) * (0.5 + vAlpha)
     mSy = (1 / (1 - phi ** 2))*((1/12) + mSig)
 
+    # Definition (4)
     mY = np.zeros((iT, iN))
 
-    # Row vector with dimension of iN, elements are generated with Norm(mu = 0, sd = 1)
-    vU = np.dot(np.random.normal(0, 1, iN), sqrtm(mSy))
+    # Definition (16)
+    vU = np.dot(np.random.normal(0, 1, iN), scipy.linalg.sqrtm(mSy))
     mY[0] = vU + vMy.T
     mH = mSig
 
     for t in range(1, iT):
+        # Definition (12)
         mH = mK + np.dot(np.dot(mC, np.outer(vU, vU)), mC) + \
             np.dot(np.dot(mD, mH), mD)
-        vU = np.dot(np.random.normal(0, 1, iN), sqrtm(mH))
+
+        # Definition (16)
+        vU = np.dot(np.random.normal(0, 1, iN),
+                    scipy.linalg.sqrtm(mH))
         mY[t] = vAlpha.T + (phi * mY[t-1]) + (beta * mX[t]) + vU
 
     return mY, mX
--- src/panel_garch.py
+++ src/panel_garch.py
@@ -4,7 +4,7 @@ import time
 import math
 import random
 import scipy.optimize
-from . import unvech, vech
+from . import unvech, vech, Obj_pg, DGP
 
 
 class panel_garch:
@@ -91,49 +91,7 @@ class panel_garch:
         return radius
 
     def DataGeneratingProcess(self, iI=0):
-        # > "Python uses the Mersenne Twister as the core generator."
-        # > https://docs.python.org/2/library/random.html
-        random.seed(123 + 2 * iI)
-
-        phi, beta = self.vTheta
-        gam, rho, varphi, eta = self.vLambda
-        mSig = self.unvech(self.vSigma)
-
-        # Definition (13)
-        mC = np.full((self.iN, self.iN), rho) + \
-            (gam - rho) * np.identity(self.iN)
-        mD = np.full((self.iN, self.iN), eta) + \
-            (varphi - eta) * np.identity(self.iN)
-
-        # Equation (14)
-        mK = mSig - np.dot(np.dot(mC, mSig), mC) - np.dot(np.dot(mD, mSig), mD)
-
-        # Matrix with size (iT * iN), elements are generated with Uniform dist over [0, 1)
-        mX = np.random.rand(self.iT, self.iN)
-
-        # Equation (15)
-        vMy = (1 / (1 - phi)) * (0.5 + self.vAlpha)
-        mSy = (1 / (1 - phi ** 2))*((1/12) + mSig)
-
-        # Definition (4)
-        mY = np.zeros((self.iT, self.iN))
-
-        # Definition (16)
-        vU = np.dot(np.random.normal(0, 1, self.iN), scipy.linalg.sqrtm(mSy))
-        mY[0] = vU + vMy.T
-        mH = mSig
-
-        for t in range(1, self.iT):
-            # Definition (12)
-            mH = mK + np.dot(np.dot(mC, np.outer(vU, vU)), mC) + \
-                np.dot(np.dot(mD, mH), mD)
-
-            # Definition (16)
-            vU = np.dot(np.random.normal(0, 1, self.iN),
-                        scipy.linalg.sqrtm(mH))
-            mY[t] = self.vAlpha.T + (phi * mY[t-1]) + (beta * mX[t]) + vU
-
-        return mY, mX
+        return DGP.DGP(self.vTheta, self.vAlpha, self.vSigma, self.vLambda, self.iT, self.iN, iI)
 
     def parser(self):
 
